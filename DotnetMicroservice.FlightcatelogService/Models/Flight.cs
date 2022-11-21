4d741bb96c34d71d368e21e4b35fd081ed827ea0
vithushanms <msvithushan@gamil.com>
2022-11-21 23:01:29 +0530

    adding models for flight catelog service

---
+++ DotnetMicroservice.FlightcatelogService/Models/Flight.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotnetMicroservice.FlightcatelogService.Models
+{
+    public class Flight
+    {
+        [Key]
+        [Required]
+        public int Id {get; set;}
+        [Required]
+        public int FlightNumber { get; set; }
+        [Required]
+        public string FlightName { get; set; }
+        [Required]
+        public DateTime DepartureTime { get; set; }
+        [Required]
+        public DateTime TakeOffTime { get; set; }
+        [Required]
+        public string Destination { get; set; }
+    }
+}
+
